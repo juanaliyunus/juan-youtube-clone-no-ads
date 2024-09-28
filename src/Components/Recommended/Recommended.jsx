ba77ebfcb993b7a977855f8d1616d99ab6c01844
enigma <aliyunusjuan@gmail.com>
2024-09-28 13:22:02 +0700

    project done with MVP

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Recommended.css";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
-import { API_KEY } from "../../data";
+import { API_KEY, value_converter } from "../../data";
+import { Link } from "react-router-dom";
 
 const Recommended = ({ categoryId }) => {
   const [apiData, setApiData] = useState([]);
@@ -26,14 +19,14 @@ const Recommended = ({ categoryId }) => {
     <div className="recommended">
       {apiData.map((item, index) => {
         return (
-          <div className="side-video-list" key={index}>
-            <img src={thumbnail1} alt="" />
+          <Link to={`/video/${item.snippet.categoryId}/${item.id}`} className="side-video-list" key={index}>
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
             <div className="vid-info">
               <h4>{item.snippet.title}</h4>
               <p>{item.snippet.channelTitle}</p>
-              <p>{item.statistics.viewCount} Views</p>
+              <p>{value_converter(item.statistics.viewCount)} Views</p>
             </div>
-          </div>
+          </Link>
         );
       })}
     </div>
