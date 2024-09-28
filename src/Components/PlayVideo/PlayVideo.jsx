8332b9b115756622c4ff32bb205ab54c1c71204e
enigma <aliyunusjuan@gmail.com>
2024-09-28 12:54:43 +0700

    almost done

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PlayVideo.css";
 import video1 from "../../assets/video.mp4";
 import like from "../../assets/like.png";
@@ -8,21 +8,63 @@ import save from "../../assets/save.png";
 import jack from "../../assets/jack.png";
 import user_profile from "../../assets/user_profile.jpg";
 import juan from "../../assets/juan.png";
+import { API_KEY, value_converter } from "../../data";
+import moment from "moment";
 
-const PlayVideo = () => {
+const PlayVideo = ({ videoId }) => {
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData, setCommentData] = useState([]);
+  const fetchVideoData = async () => {
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDetails_url)
+      .then((rest) => rest.json())
+      .then((data) => setApiData(data.items[0]));
+  };
+
+  const fetchOtherData = async () => {
+    const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
+    await fetch(channelData_url)
+      .then((res) => res.json())
+      .then((data) => setChannelData(data.items[0]));
+
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`;
+
+    await fetch(comment_url)
+      .then((res) => res.json())
+      .then((data) => setCommentData(data.items));
+  };
+
+  useEffect(() => {
+    fetchVideoData();
+  }, []);
+
+  useEffect(() => {
+    fetchOtherData();
+  }, [apiData]);
   return (
     <div className="play-video">
-      <video src={video1} controls autoPlay muted></video>
-      <h3>Best YouTube Channel to learn coding</h3>
+      {/* <video src={video1} controls autoPlay muted></video> */}
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
       <div className="play-video-info">
-        <p>1525 Views &bull; 2 days ago</p>
+        <p>
+          {apiData ? value_converter(apiData.statistics.viewCount) : "16K"}{" "}
+          &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}
+        </p>
         <div>
           <span>
             <img src={like} alt="Like" />
-            125
+            {apiData ? value_converter(apiData.statistics.likeCount) : 155}
           </span>
           <span>
-            <img src={dislike} alt="Dislike" /> 2
+            <img src={dislike} alt="Dislike" />
           </span>
           <span>
             <img src={share} alt="Share" />
@@ -36,102 +78,50 @@ const PlayVideo = () => {
       </div>
       <hr />
       <div className="publisher">
-        <img src={juan} alt="Juan" />
+        <img
+          src={channelData ? channelData.snippet.thumbnails.default.url : ""}
+          alt="Juan"
+        />
         <div>
-          <p>GreatStack</p>
-          <span>1M Subscribers</span>
+          <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
+          <span>
+            {channelData
+              ? value_converter(channelData.statistics.subscriberCount)
+              : "1M"}{" "}
+            Subscribers
+          </span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-description">
-        <p>Channel that makes learning Easy</p>
-        <p>Subscribe GreatStack to Watch More Tutorials on web development</p>
+        <p>
+          {apiData
+            ? apiData.snippet.description.slice(0, 250)
+            : "Description Here"}
+        </p>
         <hr />
-        <h4>130 Comments</h4>
-        <div className="comment">
-          <img src={user_profile} alt="User Profile" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>Great video, thanks for the tips!</p>
-            <div className="comment-action">
-              <img src={like} alt="Like" />
-              <span>232</span>
-              <img src={dislike} alt="Dislike" />
+        <h4>
+          {apiData ? value_converter(apiData.statistics.commentCount) : 117}{" "}
+          Comments
+        </h4>
+        {commentData.map((item, index) => {
+          return (
+            <div key={index} className="comment">
+              <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="User Profile" />
+              <div>
+                <h3>
+                  {item.snippet.topLevelComment.snippet.authorDisplayName} <span>1 day ago</span>
+                </h3>
+                <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
+                <div className="comment-action">
+                  <img src={like} alt="Like" />
+                  <span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                  <img src={dislike} alt="Dislike" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="User Profile" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>Great video, thanks for the tips!</p>
-            <div className="comment-action">
-              <img src={like} alt="Like" />
-              <span>232</span>
-              <img src={dislike} alt="Dislike" />
-            </div>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="User Profile" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>Great video, thanks for the tips!</p>
-            <div className="comment-action">
-              <img src={like} alt="Like" />
-              <span>232</span>
-              <img src={dislike} alt="Dislike" />
-            </div>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="User Profile" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>Great video, thanks for the tips!</p>
-            <div className="comment-action">
-              <img src={like} alt="Like" />
-              <span>232</span>
-              <img src={dislike} alt="Dislike" />
-            </div>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="User Profile" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>Great video, thanks for the tips!</p>
-            <div className="comment-action">
-              <img src={like} alt="Like" />
-              <span>232</span>
-              <img src={dislike} alt="Dislike" />
-            </div>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="User Profile" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>Great video, thanks for the tips!</p>
-            <div className="comment-action">
-              <img src={like} alt="Like" />
-              <span>232</span>
-              <img src={dislike} alt="Dislike" />
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -19,31 +19,31 @@ const Sidebar = ({sidebar, category, setCategory}) => {
   return (
     <div className={`sidebar ${sidebar ? '' : 'small-sidebar'}`}>
       <div className="shortcut-links">
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===0?"active":""}`} onClick={() => setCategory(0)}>
           <img src={home} alt="Home" /> <p>Home</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===20?"active":""}`} onClick={() => setCategory(20)}>
           <img src={game_icon} alt="Gaming" /> <p>Gaming</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===2?"active":""}`} onClick={() => setCategory(2)}>
           <img src={automobiles} alt="Automobiles" /> <p>Automobiles</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===17?"active":""}`} onClick={() => setCategory(17)}>
           <img src={sports} alt="Sports" /> <p>Sports</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===24?"active":""}`} onClick={() => setCategory(24)}>
           <img src={entertainment} alt="Entertainment" /> <p>Entertainment</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===28?"active":""}`} onClick={() => setCategory(28)}>
           <img src={tech} alt="Technology" /> <p>Technology</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===10?"active":""}`} onClick={() => setCategory(10)}>
           <img src={music} alt="Music" /> <p>Music</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===22?"active":""}`} onClick={() => setCategory(22)}>
           <img src={blogs} alt="Blogs" /> <p>Blogs</p>
         </div>
-        <div className="side-link" onClick={() => setCategory(0)}>
+        <div className={`side-link ${category===25?"active":""}`} onClick={() => setCategory(25)}>
           <img src={news} alt="News" /> <p>News</p>
         </div>
         <hr />
